0062c34d19a6bb3db83d4d9b06534ae6e7aeb91d
Matus Chochlik <chochlik@gmail.com>
2021-06-11 07:56:47 +0200

    Multiple changes
    - Updated the submodules.
    - Added support for embedded CA certificate
    - Added library where the resources are embedded.
    - Added a new example showing how to get the default certificate.
    - Removed the version file.
    - Updated the YCM config.

---
+++ include/eagine/ssl_resources.hpp
@@ -0,0 +1,34 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+#ifndef EAGINE_SSL_RESOURCES_HPP
+#define EAGINE_SSL_RESOURCES_HPP
+
+#include <eagine/main_ctx_fwd.hpp>
+#include <eagine/memory/block.hpp>
+
+namespace eagine {
+//------------------------------------------------------------------------------
+auto ca_certificate_pem(
+  memory::const_block embedded_blk,
+  memory::buffer&,
+  application_config&,
+  logger&) -> memory::const_block;
+//------------------------------------------------------------------------------
+auto ca_certificate_pem(memory::const_block embedded_blk, main_ctx& ctx)
+  -> memory::const_block;
+//------------------------------------------------------------------------------
+memory::const_block ca_certificate_pem(main_ctx&);
+//------------------------------------------------------------------------------
+} // namespace eagine
+
+#if !EAGINE_SSL_LIBRARY || defined(EAGINE_IMPLEMENTING_LIBRARY)
+#include <eagine/ssl_resources.inl>
+#endif
+
+#endif // EAGINE_SSL_RESOURCES_HPP
+
