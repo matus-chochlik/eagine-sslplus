224f5198891d7b88086881610e8b9ad7fbae0c00
Matus Chochlik <chochlik@gmail.com>
2021-06-12 07:44:40 +0200

    Changed how headers are classified and handled

---
+++ include/Sources.cmake
@@ -0,0 +1,27 @@
+# Copyright Matus Chochlik.
+# Distributed under the Boost Software License, Version 1.0.
+# See accompanying file LICENSE_1_0.txt or copy at
+#  http://www.boost.org/LICENSE_1_0.txt
+#
+set(HEADERS
+    eagine/ssl_api/api.hpp
+    eagine/ssl_api/api_traits.hpp
+    eagine/ssl_api/c_api.hpp
+    eagine/ssl_api/config.hpp
+    eagine/ssl_api/constants.hpp
+    eagine/ssl_api_fwd.hpp
+    eagine/ssl_api.hpp
+    eagine/ssl_api/object_handle.hpp
+    eagine/ssl_api/object_stack.hpp
+    eagine/ssl_api/result.hpp
+    eagine/ssl.hpp
+    eagine/ssl_resources.hpp
+)
+
+set(PUB_INLS
+    eagine/ssl_api.inl
+)
+
+set(LIB_INLS
+)
+
