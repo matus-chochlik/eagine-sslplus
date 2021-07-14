9df1751cde47f5233fa2e09f26edd8fadd0afb42
Matus Chochlik <chochlik@gmail.com>
2021-07-14 18:11:04 +0200

    Added doxygen and sphinx documentation sources + updated sub-modules

---
+++ doc/doxygen/include/sslplus.hpp
@@ -0,0 +1,16 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+
+namespace eagine {
+
+/// @brief C++ wrapper core for the OpenSSL API is placed in this namespace.
+namespace sslplus {
+/// @defgroup sslplus OpenSSL API wrappers
+}
+
+} // namespace eagine
