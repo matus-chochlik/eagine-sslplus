3d97b11c74620407d4a26ecf0f9ac45cdd4920d3
Matus Chochlik <chochlik@gmail.com>
2024-06-15 07:16:35 +0200

    Changed how packages are built + updated core sub-module

---
+++ config/EAGinePackageSSLplus.cmake
@@ -0,0 +1,12 @@
+#  Copyright Matus Chochlik.
+#  Distributed under the Boost Software License, Version 1.0.
+#  See accompanying file LICENSE_1_0.txt or copy at
+#  https://www.boost.org/LICENSE_1_0.txt
+#
+# Package specific options
+#  Debian
+#   Dependencies
+set(CPACK_DEBIAN_SSLPLUS-DEV_PACKAGE_DEPENDS "libssl3,eagine-core-dev (>= @EAGINE_VERSION@)")
+#   Descriptions
+set(CPACK_DEBIAN_SSLPLUS-DEV_DESCRIPTION "")
+
