58fdb5e74599e55e305fdc30b23cda738c1b1dae
Matus Chochlik <chochlik@gmail.com>
2024-06-16 11:24:21 +0200

    Added the examples install component + updated sub-modules

--- config/EAGinePackageSSLplus.cmake
+++ config/EAGinePackageSSLplus.cmake
@@ -6,7 +6,10 @@
 # Package specific options
 #  Debian
 #   Dependencies
+set(CXX_RUNTIME_PKGS "libc6,libc++1-17")
+set(CPACK_DEBIAN_SSLPLUS-EXAMPLES_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g,libssl3")
 set(CPACK_DEBIAN_SSLPLUS-DEV_PACKAGE_DEPENDS "libssl3,eagine-core-dev (>= @EAGINE_VERSION@)")
 #   Descriptions
-set(CPACK_DEBIAN_SSLPLUS-DEV_DESCRIPTION "")
+set(CPACK_DEBIAN_SSLPLUS-EXAMPLES_DESCRIPTION "EAGine SSLplus examples")
+set(CPACK_DEBIAN_SSLPLUS-DEV_DESCRIPTION "C++ wrapper for OpenSSL")
 
