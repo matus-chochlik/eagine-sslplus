57121bae60b13f02cd10ef87cc56942cede62842
Matus Chochlik <chochlik@gmail.com>
2024-06-19 10:50:50 +0200

    Changes in how package properties are specified + updated sub-modules

--- config/EAGinePackageSSLplus.cmake
+++ config/EAGinePackageSSLplus.cmake
@@ -3,13 +3,5 @@
 #  See accompanying file LICENSE_1_0.txt or copy at
 #  https://www.boost.org/LICENSE_1_0.txt
 #
-# Package specific options
-#  Debian
-#   Dependencies
-set(CXX_RUNTIME_PKGS "libc6,libc++1-17")
-set(CPACK_DEBIAN_SSLPLUS-EXAMPLES_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g,libssl3")
-set(CPACK_DEBIAN_SSLPLUS-DEV_PACKAGE_DEPENDS "libssl3,eagine-core-dev (>= @EAGINE_VERSION@)")
-#   Descriptions
-set(CPACK_DEBIAN_SSLPLUS-EXAMPLES_DESCRIPTION "EAGine SSLplus examples")
-set(CPACK_DEBIAN_SSLPLUS-DEV_DESCRIPTION "C++ wrapper for OpenSSL")
-
+file(APPEND "${CMAKE_BINARY_DIR}/CPackPropertiesEAGine.cmake"
+	"set(EAGINE_SSLPLUS_RUNTIME_PKGS \"libssl3\")\n")
